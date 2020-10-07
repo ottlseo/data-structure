05385b32479cbe5f97776e8711b47dc64948b58a
Yoonseo KIM <61778930+yoonseo-hi@users.noreply.github.com>
2020-10-07 17:32:40 +0900

    과제 제출 양식

---
+++ form.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void printInfo();
+
+int main() {
+	printInfo(); //정보 출력
+
+
+	return 0;
+}
+void printInfo(void) {
+	printf("====================\n");
+	printf("학과: 사이버보안전공\n");
+	printf("학번: 1971063\n");
+	printf("성명: 김윤서\n");
+	printf("====================\n");
+}
